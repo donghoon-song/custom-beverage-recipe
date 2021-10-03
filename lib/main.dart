12ebaa40a5abe416beef4c67d24d07850c970ce4
heewookji <wlgmldnr@daum.net>
2021-10-03 16:58:06 +0900

    chore: firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,20 @@
 import 'package:cubere/config/provider_logger.dart';
 import 'package:cubere/view/app.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() {
+void main() async {
+  await initializeServices();
   runApp(
     ProviderScope(
       child: const App(),
       observers: [ProviderLogger()],
     ),
   );
+}
+
+Future<void> initializeServices() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
 }
