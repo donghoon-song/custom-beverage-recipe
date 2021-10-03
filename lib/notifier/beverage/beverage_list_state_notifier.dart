e63ad2f636c1427d7508266e1e7fe9eca8fc0dcf
heewookji <wlgmldnr@daum.net>
2021-10-03 19:21:45 +0900

    fix: remove autoDispose

--- lib/notifier/beverage/beverage_list_state_notifier.dart
+++ lib/notifier/beverage/beverage_list_state_notifier.dart
@@ -1,22 +1,14 @@
-import 'dart:async';
-
 import 'package:cubere/data/repository/beverage_repository.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'beverage_list_state.dart';
 
-final beverageListProvider = StateNotifierProvider.autoDispose<
-    BeverageListStateNotifier, BeverageListState>((ref) {
-  final beverageListStateNotifier = BeverageListStateNotifier(ref);
-  ref.onDispose(() {
-    beverageListStateNotifier._beverageListSubscription.cancel();
-  });
-  return beverageListStateNotifier;
-});
+final beverageListProvider =
+    StateNotifierProvider<BeverageListStateNotifier, BeverageListState>(
+        (ref) => BeverageListStateNotifier(ref));
 
 class BeverageListStateNotifier extends StateNotifier<BeverageListState> {
   final BeverageRepository _beverageRepository;
-  late StreamSubscription _beverageListSubscription;
 
   BeverageListStateNotifier(ProviderRefBase ref)
       : _beverageRepository = ref.read(beverageRepositoryProvider),
@@ -34,7 +26,7 @@ class BeverageListStateNotifier extends StateNotifier<BeverageListState> {
           serverError: () => '서버 오류입니다.',
         ),
       ),
-      (r) => _beverageListSubscription = r.listen((beverageList) {
+      (r) => r.listen((beverageList) {
         state = BeverageListState.loaded(beverageList);
       }),
     );
