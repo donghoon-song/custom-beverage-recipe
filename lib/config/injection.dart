de3b991dc3b2a34e37ba69b5336e14f9ca93699f
heewookji <wlgmldnr@daum.net>
2021-10-09 22:26:01 +0900

    feat: firebaseAuth setup

--- lib/config/injection.dart
+++ lib/config/injection.dart
@@ -1,7 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:logger/logger.dart';
 
 final loggerProvider = Provider<Logger>((ref) => Logger());
 final firestoreProvider =
     Provider<FirebaseFirestore>((ref) => FirebaseFirestore.instance);
+final firebaseAuthProvider =
+    Provider<FirebaseAuth>((ref) => FirebaseAuth.instance);
