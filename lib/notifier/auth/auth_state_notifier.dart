1f4d5511c2ce68881b48c92d4dac54aff08b9779
heewookji <wlgmldnr@daum.net>
2021-10-17 21:55:03 +0900

    add: ios apple sign in

--- lib/notifier/auth/auth_state_notifier.dart
+++ lib/notifier/auth/auth_state_notifier.dart
@@ -1,7 +1,12 @@
+import 'dart:convert';
+import 'dart:math';
+
+import 'package:crypto/crypto.dart';
 import 'package:cubere/config/injection.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:sign_in_with_apple/sign_in_with_apple.dart';
 
 import 'auth_state.dart';
 
@@ -9,18 +14,18 @@ final authProvider = StateNotifierProvider<AuthStateNotifier, AuthState>(
     (ref) => AuthStateNotifier(ref));
 
 class AuthStateNotifier extends StateNotifier<AuthState> {
-  final FirebaseAuth _firebaseAuthProvider;
-  final GoogleSignIn _googleSignInProvider;
+  final FirebaseAuth _firebaseAuth;
+  final GoogleSignIn _googleSignIn;
 
   AuthStateNotifier(ProviderRefBase ref)
-      : _firebaseAuthProvider = ref.read(firebaseAuthProvider),
-        _googleSignInProvider = ref.read(googleSignInProvider),
+      : _firebaseAuth = ref.read(firebaseAuthProvider),
+        _googleSignIn = ref.read(googleSignInProvider),
         super(const AuthState.unknown()) {
     _listenToFirebaseAuth();
   }
 
   _listenToFirebaseAuth() {
-    final authStream = _firebaseAuthProvider.authStateChanges();
+    final authStream = _firebaseAuth.authStateChanges();
     authStream.listen((user) {
       if (user == null) {
         state = const AuthState.unknown();
@@ -31,22 +36,56 @@ class AuthStateNotifier extends StateNotifier<AuthState> {
   }
 
   signIn() {
-    _signInWithGoogle();
+    // _signInWithGoogle();
+    _signInWithApple();
   }
 
   signOut() {
-    _firebaseAuthProvider.signOut();
+    _firebaseAuth.signOut();
   }
 
   Future<UserCredential> _signInWithGoogle() async {
-    final GoogleSignInAccount? googleUser =
-        await _googleSignInProvider.signIn();
+    final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
     final GoogleSignInAuthentication? googleAuth =
         await googleUser?.authentication;
     final credential = GoogleAuthProvider.credential(
       accessToken: googleAuth?.accessToken,
       idToken: googleAuth?.idToken,
     );
-    return _firebaseAuthProvider.signInWithCredential(credential);
+    return _firebaseAuth.signInWithCredential(credential);
+  }
+
+  Future<UserCredential> _signInWithApple() async {
+    final rawNonce = generateNonce();
+    final nonce = sha256ofString(rawNonce);
+
+    final appleCredential = await SignInWithApple.getAppleIDCredential(
+      scopes: [
+        AppleIDAuthorizationScopes.email,
+        AppleIDAuthorizationScopes.fullName,
+      ],
+      nonce: nonce,
+    );
+
+    final oauthCredential = OAuthProvider("apple.com").credential(
+      idToken: appleCredential.identityToken,
+      rawNonce: rawNonce,
+    );
+
+    return _firebaseAuth.signInWithCredential(oauthCredential);
+  }
+
+  String generateNonce([int length = 32]) {
+    const charset =
+        '0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._';
+    final random = Random.secure();
+    return List.generate(length, (_) => charset[random.nextInt(charset.length)])
+        .join();
+  }
+
+  String sha256ofString(String input) {
+    final bytes = utf8.encode(input);
+    final digest = sha256.convert(bytes);
+    return digest.toString();
   }
 }
