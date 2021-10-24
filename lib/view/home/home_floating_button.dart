e65fb6f192b5be80bcee61bd8ea3a6b6c4abed7e
heewookji <wlgmldnr@daum.net>
2021-10-24 16:52:48 +0900

    feat: add animated floating menu

--- lib/view/home/home_floating_button.dart
+++ lib/view/home/home_floating_button.dart
@@ -1,4 +1,6 @@
+import 'package:cubere/notifier/auth/auth_state.dart';
 import 'package:cubere/notifier/auth/auth_state_notifier.dart';
+import 'package:fab_circular_menu/fab_circular_menu.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -10,13 +12,35 @@ class HomeFloatingButton extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final auth = ref.watch(authProvider);
     final authAction = ref.watch(authProvider.notifier);
+    final theme = Theme.of(context);
+    final deviceSize = MediaQuery.of(context).size;
+    return FabCircularMenu(
+      fabColor: Colors.white,
+      ringColor: theme.primaryColor.withOpacity(0.1),
+      ringDiameter: deviceSize.width * 0.7,
+      ringWidth: deviceSize.width * 0.7 * 0.2,
+      children: <Widget>[
+        _buildCreateButton(),
+        _buildAuthButton(auth, authAction),
+      ],
+    );
+  }
+
+  IconButton _buildCreateButton() {
+    return IconButton(
+      icon: const Icon(Icons.add),
+      onPressed: () {},
+    );
+  }
+
+  IconButton _buildAuthButton(AuthState auth, AuthStateNotifier authAction) {
     return auth.when(
-      unknown: () => FloatingActionButton(
-        child: const Icon(Icons.login_rounded),
+      unknown: () => IconButton(
+        icon: const Icon(Icons.login_rounded),
         onPressed: () => authAction.signIn(),
       ),
-      authenticated: (user) => FloatingActionButton(
-        child: const Icon(Icons.logout_rounded),
+      authenticated: (user) => IconButton(
+        icon: const Icon(Icons.logout_rounded),
         onPressed: () => authAction.signOut(),
       ),
     );
