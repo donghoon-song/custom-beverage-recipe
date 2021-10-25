a64055c80915a4e326e9f22cf9c3976a7f569f96
heewookji <wlgmldnr@daum.net>
2021-10-25 22:58:23 +0900

    style: add ui const class

---
+++ lib/common/const/ui.dart
@@ -0,0 +1,6 @@
+class UI {
+  static const double bottomSheetHeight = 200;
+  static const double cardRadius = 16;
+  static const double cardPadding = 16;
+  static const double iconRadius = 50;
+}
